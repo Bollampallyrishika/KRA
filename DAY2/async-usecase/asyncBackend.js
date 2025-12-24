64dbc68a3ad9c57ade9ac989ee30577fe4244035
Rishika Bollampally <rishika@medianv.com>
2025-12-24 11:04:16 +0530

    added comment structure

--- DAY2/async-usecase/asyncBackend.js
+++ DAY2/async-usecase/asyncBackend.js
@@ -1,5 +1,5 @@
-//An async backend handles requests in a non-blocking way, allowing 
-// the server to process many requests concurrently without waiting for each task to finish.
+/*An async backend handles requests in a non-blocking way, allowing 
+the server to process many requests concurrently without waiting for each task to finish.*/
 
 const fs = require("fs");
 
